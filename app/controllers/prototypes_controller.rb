95b94f9cc1453aceef1fba476fa48602f2fe1971
yuki986 <yuki.kuribayashi8@gmail.com>
2021-01-15 19:21:20 +0900

    詳細

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -16,6 +16,10 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def show
+    @prototype = Prototype.find(params [:id])
+  end
+
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
