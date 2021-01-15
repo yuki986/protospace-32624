91cadeba1da69a53a251ddc1d8832bf27a7232ba
yuki986 <yuki.kuribayashi8@gmail.com>
2021-01-15 12:03:01 +0900

    prototypesのnewとcleateをルーティングとコントローラーにつける。

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,19 @@
 class PrototypesController < ApplicationController
   def index
+    @prototypes = Prototype.all
   end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def cleate
+    prototype.create(prototype_params)
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
+  end
+
 end
